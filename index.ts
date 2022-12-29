ef589155e4308feccd5f1dba9020e2be31018651
Zwyx <29386932+Zwyx@users.noreply.github.com>
2022-12-29 19:38:23 +0800

    Switch to tabs for indentation
    Following our discussion and this article:
    https://www.reddit.com/r/javascript/comments/c8drjo/nobody_talks_about_the_real_reason_to_use_tabs/
    
    Interesting note: `npm` automatically convert `package-lock.json` to tabs if `package.json` contains tabs.
    
    Also add `.editorconfig` for charset and end_of_line

--- index.ts
+++ index.ts
@@ -6,64 +6,64 @@ import { simpleGit } from "simple-git";
 import { getCommitList } from "./getCommitList";
 
 const { repositories, emails, remote } = JSON.parse(
-  readFileSync("./repos.json").toString()
+	readFileSync("./repos.json").toString(),
 );
 
 if (!Array.isArray(repositories) || !Array.isArray(emails)) {
-  console.error(
-    "Expecting a './repos.json' with an array 'repositories' and 'emails'. See 'repos.example.json'"
-  );
-  process.exit(-1);
+	console.error(
+		"Expecting a './repos.json' with an array 'repositories' and 'emails'. See 'repos.example.json'",
+	);
+	process.exit(-1);
 }
 
 (async () => {
-  const allCommits = [];
-  for (const repoUrl of repositories) {
-    allCommits.push(...(await getCommitList(repoUrl, emails)));
-  }
+	const allCommits = [];
+	for (const repoUrl of repositories) {
+		allCommits.push(...(await getCommitList(repoUrl, emails)));
+	}
 
-  const sortedCommits = R.sortBy(
-    ({ date }) => new Date(date).getTime(),
-    allCommits
-  );
+	const sortedCommits = R.sortBy(
+		({ date }) => new Date(date).getTime(),
+		allCommits,
+	);
 
-  // Useful to show what we've found, try dropping of the author filter and logging it.
-  // console.log(R.uniq(allCommits.map(({ email }) => email)));
+	// Useful to show what we've found, try dropping of the author filter and logging it.
+	// console.log(R.uniq(allCommits.map(({ email }) => email)));
 
-  const TEST_REPO = path.join(__dirname, "./test-git-repo");
-  // delete if it exists
-  try {
-    rmSync(TEST_REPO, { recursive: true });
-  } catch {
-    // we don't mind if it was already missing
-  }
+	const TEST_REPO = path.join(__dirname, "./test-git-repo");
+	// delete if it exists
+	try {
+		rmSync(TEST_REPO, { recursive: true });
+	} catch {
+		// we don't mind if it was already missing
+	}
 
-  mkdirSync(TEST_REPO);
+	mkdirSync(TEST_REPO);
 
-  const git = simpleGit({
-    baseDir: TEST_REPO,
-  });
+	const git = simpleGit({
+		baseDir: TEST_REPO,
+	});
 
-  await git.init();
+	await git.init();
 
-  if (!!remote) {
-    await git.addRemote("origin", remote);
-  }
+	if (!!remote) {
+		await git.addRemote("origin", remote);
+	}
 
-  console.log('Starting to commit to "./test-git-repo"');
+	console.log('Starting to commit to "./test-git-repo"');
 
-  process.chdir(TEST_REPO);
+	process.chdir(TEST_REPO);
 
-  for (const commit of sortedCommits) {
-    const gitCommand = `GIT_AUTHOR_DATE="${commit.date}" GIT_COMMITTER_DATE="${commit.date}" git commit --allow-empty --no-gpg-sign -m "Another commit"`;
+	for (const commit of sortedCommits) {
+		const gitCommand = `GIT_AUTHOR_DATE="${commit.date}" GIT_COMMITTER_DATE="${commit.date}" git commit --allow-empty --no-gpg-sign -m "Another commit"`;
 
-    execSync(gitCommand, { encoding: "utf8" });
+		execSync(gitCommand, { encoding: "utf8" });
 
-    process.stdout.write(".");
-  }
+		process.stdout.write(".");
+	}
 
-  console.log();
-  console.log(
-    `Added ${sortedCommits.length} contributions found in ${repositories.length} repositories`
-  );
+	console.log();
+	console.log(
+		`Added ${sortedCommits.length} contributions found in ${repositories.length} repositories`,
+	);
 })();
